e5d7197a175ebce9cb42ed282ed9d73ccbb34ac0
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-03-02 00:48:03 +0100

    feat: description core bussiness product

--- product/domain/entities/product.go
+++ product/domain/entities/product.go
@@ -1,1 +1,38 @@
 package entities
+
+import (
+	"errors"
+	"github.com/asaskevich/govalidator"
+)
+
+type Product struct {
+	Name  string  `json:"name"`
+	Price float64 `json:"price,string"`
+}
+
+func NewProduct(name string, price float64) (*Product, error) {
+	products := &Product{
+		Name:  name,
+		Price: price,
+	}
+	err := products.priceValid()
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func (p *Product) priceValid() error {
+	if p.Price <= 0 {
+		return errors.New("price not lower the 0")
+	}
+	return nil
+}
+
+func (p *Product) validate() error {
+	_, err := govalidator.ValidateStruct(p)
+	if err != nil {
+		return err
+	}
+	return nil
+}
