785b7e1050ae136116872bccb134d07a88f53109
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-03-02 01:15:28 +0100

    test: core bussiness in product :green #description core bussiness product

--- product/domain/entities/product_test.go
+++ product/domain/entities/product_test.go
@@ -1,1 +1,14 @@
 package entities_test
+
+import (
+	"github.com/bxcodec/faker/v3"
+	"github.com/stretchr/testify/assert"
+	"product/domain/entities"
+	"testing"
+)
+
+func TestProductValid(t *testing.T) {
+	product, err := entities.NewProduct(faker.NAME, 1000)
+	assert.Nil(t, err)
+	assert.Equal(t, product.Price, 1000.0)
+}
